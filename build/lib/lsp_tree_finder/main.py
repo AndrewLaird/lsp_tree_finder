6a3d7356ea51facf1ba2fcdc38391194924980f3
Andrew Laird <lairdandrew11@gmail.com>
2023-04-24 11:24:26 -0700

    working, still debug at the top

--- build/lib/lsp_tree_finder/main.py
+++ build/lib/lsp_tree_finder/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import argparse
 import re
 from typing import List, Optional, Tuple
@@ -9,21 +10,21 @@ from lsp_tree_finder.helpers import lsp, treesitter
 import pylspclient
 from pylspclient.lsp_client import lsp_structs
 
-
 Language.build_library(
     # Store the library in the `build` directory
-    "build/my-languages.so",
+    str(Path(__file__).parent / "build/my-languages.so"),
     # Include one or more languages
     [
         str(Path(__file__).parent / "vendor/tree-sitter-php"),
     ],
 )
 
-PHP_LANGUAGE = Language("build/my-languages.so", "php")
+PHP_LANGUAGE = Language(str(Path(__file__).parent / "build/my-languages.so"), "php")
 
 parser = Parser()
 parser.set_language(PHP_LANGUAGE)
 parsed_files = {}
+failed_to_follow = set()
 
 
 def parse_file(file_path):
@@ -39,20 +40,44 @@ def parse_file(file_path):
 
 
 class PathObject:
-    def __init__(self, file_name, function_name, start_line, match_line_number):
+    def __init__(
+        self,
+        file_name: str,
+        function_name: str,
+        start_line: int,
+        path_function_call_line: int,
+    ):
         self.file_name = file_name
         self.function_name = function_name
         self.start_line = start_line
-        self.match_line_number = match_line_number
+        self.path_function_call_line = path_function_call_line
 
     def __str__(self):
-        return f"{self.file_name}: {self.function_name} (line {self.match_line_number})"
+        return f"{self.file_name}: {self.function_name} (line {self.path_function_call_line})"
 
     def __repr__(self):
         return self.__str__()
 
 
-def get_function_or_method_name(node):
+class MatchObject:
+    def __init__(
+        self,
+        node: treesitter.Node,
+        file_name: str,
+        function_text: str,
+        function_line_number: int,
+        match_line_number: int,
+        path: List[PathObject],
+    ):
+        self.node = node
+        self.file_name = file_name
+        self.function_text = function_text
+        self.function_line_number = function_line_number
+        self.match_line_number = match_line_number
+        self.path = path
+
+
+def get_function_or_method_name(node) -> str:
     def find_name(child_node):
         if child_node.type == "name":
             return child_node.text
@@ -63,7 +88,7 @@ def get_function_or_method_name(node):
                 return result.decode()
 
     if node.type != "method_declaration":
-        return None
+        return "Not called on a method"
 
     return find_name(node)
 
@@ -88,10 +113,6 @@ def find_function_or_method(node, name):
     return None
 
 
-def find_function_or_method_by_name(code, name):
-    return find_function_or_method(root_node, name)
-
-
 def collect_function_calls(
     lsp_client,
     node,
@@ -100,6 +121,7 @@ def collect_function_calls(
     visited_nodes,
     path: List[PathObject],
     file_name: str,
+    function_node: treesitter.Node,
 ):
     if not (file_name, node) or (file_name, node.id) in visited_nodes:
         return
@@ -108,7 +130,6 @@ def collect_function_calls(
     if node.type in ["function_call", "method_declaration"]:
         text = node.text
 
-        function_name = get_function_or_method_name(node)
         function_start_line = node.start_point[0] + 1
         search_results = pattern.finditer(text.decode())
         for search_result in search_results:
@@ -120,17 +141,15 @@ def collect_function_calls(
             # Calculate the line number of the match in the whole file
             match_line_number = function_start_line + lines_before_match
 
-            path_object = PathObject(
-                file_name, function_name, function_start_line, match_line_number
-            )
-            path.append(path_object)
             matches.append(
-                {
-                    "node": node,
-                    "text": text.decode(),
-                    "name": path_object.file_name,
-                    "path": path.copy(),
-                }
+                MatchObject(
+                    node=node,
+                    file_name=os.path.relpath(file_name, os.getcwd()),
+                    function_text=text.decode(),
+                    function_line_number=function_start_line,
+                    match_line_number=match_line_number,
+                    path=path,
+                )
             )
 
     if node.type in ["member_call_expression", "object_creation_expression"]:
@@ -138,28 +157,42 @@ def collect_function_calls(
         get_definition_result = get_definition_node_of_member_call_expression(
             lsp_client, node, file_name
         )
-        if get_definition_result is None:
-            function_name_node = treesitter.find_child_of_type(node, "name")
-            print("Could not get definition for", str(function_name_node.text))
-        else:
+        if get_definition_result is not None:
             target_node, target_file_path = get_definition_result
 
             if target_node:
+                path_object = PathObject(
+                    file_name=os.path.relpath(file_name, os.getcwd()),
+                    function_name=get_function_or_method_name(function_node),
+                    start_line=function_node.start_point[0] + 1,
+                    path_function_call_line=node.start_point[0] + 1,
+                )
+                target_path = path.copy()
+                target_path.append(path_object)
+
                 collect_function_calls(
                     lsp_client,
                     target_node,
                     pattern,
                     matches,
                     visited_nodes,
-                    path,
+                    target_path,
                     target_file_path,
+                    target_node,
                 )
             else:
                 print("No target node for this node")
 
     for child in node.children:
         collect_function_calls(
-            lsp_client, child, pattern, matches, visited_nodes, path, file_name
+            lsp_client,
+            child,
+            pattern,
+            matches,
+            visited_nodes,
+            path,
+            file_name,
+            function_node,
         )
 
 
@@ -176,7 +209,7 @@ def get_tree_sitter_node_from_lsp_range(
     )
     if target_node is None:
         # raise Exception("Goto not found")
-        return (target_tree.root_node, str(target_file_path))
+        return (root_node, str(target_file_path))
     else:
         return target_node, str(target_file_path)
 
@@ -199,12 +232,14 @@ def get_definition_node_of_member_call_expression(
         return None
 
     function_name_node = treesitter.find_child_of_type(node, "name")
+    if function_name_node is None:
+        return None
     start_row, start_col = function_name_node.start_point
 
     results = lsp_client.get_definitions(file_name, start_row, start_col)
 
     if not results:
-        print(":(  No definition found")
+        failed_to_follow.add(function_name_node.text.decode())
         return None
 
     tree_nodes = [get_tree_sitter_node_from_lsp_range(result) for result in results]
@@ -215,28 +250,31 @@ def get_definition_node_of_member_call_expression(
     ]
 
     if not tree_nodes:
-        print("No definition found")
         return None
 
     return tree_nodes[0]
 
 
-def print_matches(matches):
+def print_matches(matches: List[MatchObject]):
     if matches:
+        print("~~~~~~~~~~~~~~~~~")
+        print("Failed to follow:")
+        print("\n ".join([str(p) for p in failed_to_follow]))
         for match in matches:
-            match_path_end = match["path"][-1]
-            print(
-                f"{match_path_end.function_name}: [line {match_path_end.match_line_number}]"
-            )
-            print("Path:\n", " -> \n ".join(str(p) for p in match["path"]))
-            line = match_path_end.start_line
-            text_lines = match["text"].split("\n")
-            for text_line in text_lines:
-              if(line == match_path_end.match_line_number):
-                print(f"***{line} ",text_line)
-              else:
-                print(f"{line} ",text_line)
-              line += 1
+
+            print("-----------------")
+            print(match.file_name)
+            match_text_line = match.match_line_number - match.function_line_number
+            text_lines = match.function_text.split("\n")
+            print(match.function_line_number, text_lines[0])
+            print(match.match_line_number, text_lines[match_text_line])
+            print("Path:\n", " -> \n ".join([str(p) for p in match.path]))
+            # for text_line in text_lines:
+            # if(line == match_path_end.match_line_number):
+            # print(f"***{line} ",text_line)
+            # else:
+            # print(f"{line} ",text_line)
+            #   line += 1
     else:
         print("No matches found")
 
@@ -251,7 +289,7 @@ def search_pattern(lsp_client, file_path, function_name, pattern):
     # Collect matching function calls
     matches = []
     visited_nodes = set()
-    path = [get_path_object_from_node(parent_function, str(file_path))]
+    path = []
     collect_function_calls(
         lsp_client,
         parent_function,
@@ -260,6 +298,7 @@ def search_pattern(lsp_client, file_path, function_name, pattern):
         visited_nodes,
         path,
         str(file_path),
+        parent_function,
     )
     return matches
 
